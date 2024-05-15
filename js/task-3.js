8bd6f11f0f2cfdac603843b3b53791c207a8ce68
kattykatty-hub <katrinkatkatrin@yahoo.com>
2024-05-15 11:27:46 +0200

    init

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content)
+    const paddingWidth = parseFloat(padding)
+    const borderWidth = parseFloat(border)
+
+    // Розрахунок загальної ширини елемента з урахуванням box-sizing: border-box
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth
+    return Math.round(totalWidth)
+}
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
